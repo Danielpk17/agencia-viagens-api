7450718ccb6507a1f998c08ad0002e04fcdb6086
Emperror <danieldalpiva445@gmail.com>
2026-09-20 19:33:45 -0300

    fix: concede permissoes no schema public exigidas pelo PostgreSQL 15

--- scripts/01_criar_banco.sql
+++ scripts/01_criar_banco.sql
@@ -5,3 +5,9 @@ CREATE DATABASE agencia_viagens
 CREATE USER agencia_app WITH PASSWORD 'agencia123';
 
 GRANT ALL PRIVILEGES ON DATABASE agencia_viagens TO agencia_app;
+
+\connect agencia_viagens
+
+GRANT ALL ON SCHEMA public TO agencia_app;
+
+ALTER SCHEMA public OWNER TO agencia_app;
